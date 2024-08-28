39d7851b2944a58e38d7826adcd4f757ccdbd747
Sakazuki-Akainu <jujutsukaisen77700@gmail.com>
2024-08-28 08:58:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -23,7 +23,7 @@ OWNER_ID = int(os.environ.get("OWNER_ID", "5461616767"))
 PORT = os.environ.get("PORT", "8080")
 
 #Database 
-DB_URI = os.environ.get("DATABASE_URL", ""mongodb+srv://Kizaru:Kizaru@cluster0.8wgzp4n.mongodb.net/?retryWrites=true&w=majority)
+DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Kizaru:Kizaru@cluster0.8wgzp4n.mongodb.net/?retryWrites=true&w=majority")
 DB_NAME = os.environ.get("DATABASE_NAME", "Kizaru"")
 
 #SHORTLINK
