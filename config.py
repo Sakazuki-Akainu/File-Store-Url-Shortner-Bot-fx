104c43d96ac2918902beb05128538a9ca3c4d7a3
Sakazuki-Akainu <jujutsukaisen77700@gmail.com>
2024-08-28 09:15:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ PORT = os.environ.get("PORT", "8080")
 
 #Database 
 DB_URI = os.environ.get("DATABASE_URL", "mongodb+srv://Kizaru:Kizaru@cluster0.8wgzp4n.mongodb.net/?retryWrites=true&w=majority")
-DB_NAME = os.environ.get("DATABASE_NAME", "Kizaru"")
+DB_NAME = os.environ.get("DATABASE_NAME", "Kizaru")
 
 #SHORTLINK
 SHORTLINK_URL = os.environ.get('SHORTLINK_URL', "gplinks.com")
@@ -39,7 +39,7 @@ TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
 try:
     ADMINS=[]
-    for x in (os.environ.get("ADMINS", "").split()):
+    for x in (os.environ.get("ADMINS", "5461616767").split()):
         ADMINS.append(int(x))
 except ValueError:
         raise Exception("Your Admins list does not contain valid integers.")
